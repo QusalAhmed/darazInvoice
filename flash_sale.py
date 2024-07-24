0d68c5324c64005290fd50a9124d47f72adf1442
Qusal <qusal15@student.sust.edu>
2024-07-25 04:45:46 +0600

    Invoice and Flash Sale completed

---
+++ flash_sale.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import sqlite3
+import math
+
+
+def is_multi():
+    return 'multi' in variation or variation == 'f' or variation == 'fs'
+
+
+def identical(common_sku):
+    cursor.execute('SELECT identical_sku FROM identical WHERE sku = ?', (common_sku,))
+    db_sku = cursor.fetchone()
+    return db_sku[0] if db_sku else common_sku
+
+
+# connect to database
+conn = sqlite3.connect('order_details.db')
+cursor = conn.cursor()
+cursor.execute('DELETE FROM campaign_data where TRUE')
+
+# Variable
+CLEAR = 2
+min_stock = 20
+delivery_cost = 55
+
+# Read the .xls file
+df = pd.read_excel('D:/Downloads/fs.xls')
+
+for index, row in df.iterrows():
+    serial = index.__hash__() + 2
+    price = math.floor(float(row['Recommended Price'].replace('<=', '')))
+    seller_sku = row['Seller SKU'].lower()
+    product_tag, variation = seller_sku.split('-')
+    # Load the .xls file to database
+    identical_product_tag = identical(product_tag)
+    if is_multi():
+        identical_sku = 'Multi ' + identical_product_tag
+    else:
+        identical_sku = identical_product_tag
+    cursor.execute('INSERT INTO campaign_data VALUES (?, ?, ?, ?, ?, ?, ?)',
+                   (serial, product_tag.title(), variation.title(), price, 0, False, identical_sku))
+    cursor.execute('SELECT price, max_price FROM pricing WHERE product_tag = ?', (identical_product_tag,))
+    try:
+        min_price, max_price = cursor.fetchone()
+    except TypeError:
+        print('No pricing found for', product_tag)
+        continue
+    if max_price is None:
+        max_price = min_price
+    if price < min_price * (1 - CLEAR / 100):
+        df.drop(index, inplace=True)
+        print(f'{serial}. {row['Seller SKU']} is dropped Price: {price}({min_price})')
+        continue
+    elif is_multi() and price < min_price + delivery_cost:
+        df.drop(index, inplace=True)
+        print(f'{serial}. {row['Seller SKU']} is dropped Price: {price}({min_price + delivery_cost})')
+        continue
+    # Set price
+    if is_multi():
+        df.loc[index, 'Campaign Price'] = price
+        cursor.execute('UPDATE campaign_data SET campaign_price = ? WHERE serial = ?',
+                       (price, serial,))
+    else:
+        df.loc[index, 'Campaign Price'] = min(max_price, price)
+        cursor.execute('UPDATE campaign_data SET campaign_price = ? WHERE serial = ?',
+                       (min(max_price, price), serial,))
+    # Set stock
+    stock = int(row['Stock'])
+    if stock >= min_stock:
+        df.loc[index, 'Campaign stock'] = min_stock
+    else:
+        df.loc[index, 'Campaign stock'] = stock
+        print('Low Stock at', stock, 'of', seller_sku)
+    # Update status of eligible products
+    cursor.execute('UPDATE campaign_data SET status = TRUE WHERE serial = ?', (serial,))
+conn.commit()
+
+# Eliminated product check
+cursor.execute('SELECT DISTINCT product_tag FROM campaign_data WHERE status = FALSE')
+for product_tag in cursor.fetchall():
+    cursor.execute('SELECT * FROM campaign_data WHERE (product_tag, status) = (?, ?)',
+                   (product_tag[0], True))
+    if not cursor.fetchall():
+        cursor.execute('SELECT recommended_price FROM campaign_data WHERE product_tag = ?', (product_tag[0],))
+        print(product_tag, 'Permanently removed at', cursor.fetchall())
+
+print(f' Total row: {df.shape[0]} '.center(40, '━'))
+df.to_excel('D:/Downloads/fs.xlsx', index=False)
+
+# Show campaign price
+cursor.execute('SELECT DISTINCT identical_sku FROM campaign_data WHERE campaign_price != 0')
+for sku in cursor.fetchall():
+    cursor.execute('SELECT campaign_price FROM campaign_data WHERE identical_sku = ? '
+                   'AND campaign_price != 0 ORDER BY campaign_price', (sku[0],))
+    price_list = cursor.fetchall()
+    print(f'{sku[0].title()} ({price_list[0][0]} - {price_list[-1][0]}): {price_list}')
--- main.py
+++ main.py
@@ -1,36 +1,110 @@
 import io
 import os
 import PyPDF2
-import csv
+import sqlite3
+import pandas as pd
+from PyPDF2 import Transformation
 from reportlab.pdfgen import canvas
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase import pdfmetrics
+from reportlab.lib.pagesizes import A5, landscape
+from datetime import datetime
+
+# connect to database
+conn = sqlite3.connect('order_details.db')
+cursor = conn.cursor()
+cursor.execute('DELETE FROM csv_data where TRUE')
+cursor.execute('DELETE FROM pdf_data where TRUE')
+cursor.execute('DELETE FROM outside_delivery where TRUE')
 
 
-def sort_factory(order_details):
-    page_no, seller_sku = order_details
-    product_tag = seller_sku.split('-')[0].split(' ')[0].lower()
-    color_family = seller_sku.split('-')[1].split('_')[0].lower()
-    return product_tag + '-' + color_family
+# Register the font
+pdfmetrics.registerFont(TTFont('BanglaFont', 'font/Li Alinur Nakkhatra ANSI V2.ttf'))
+pdfmetrics.registerFont(TTFont('Roboto', 'font/Roboto-Regular.ttf'))
 
 
-def create_watermark(text, overlay_page_size):
+def identical(common_sku: str):
+    cursor.execute('SELECT identical_sku FROM identical WHERE sku = ?', (common_sku,))
+    identical_sku = cursor.fetchone()
+    return identical_sku[0].strip() if identical_sku else common_sku.strip()
+
+
+def sort_factory(order_details: tuple):
+    order_number, seller_skus, page_number = order_details
+    sort_text = 'Multi: ' + seller_skus if ',' in seller_skus else seller_skus
+    # for seller_sku in seller_skus.split(','):
+    #     product_tag = identical(seller_sku.split('-')[0].strip().lower())
+    #     if seller_sku != '':
+    #         color_family = identical(seller_sku.split('-')[1].split('_')[0].strip().lower())
+    #     else:
+    #         color_family = 'Unlisted'
+    #     sort_text += identical(product_tag + '-' + color_family) + ', '
+    cursor.execute('UPDATE pdf_data SET identity_sku = ? WHERE order_number = ?',
+                   (sort_text.strip(', ').title(), order_number))
+    conn.commit()
+    cursor.execute('SELECT order_time FROM csv_data WHERE order_number = ?', (order_number,))
+    return sort_text.title() + str(cursor.fetchone())
+
+
+def create_watermark(text: str, overlay_page_size: tuple, x=10, y=None, font_size=10, font='Helvetica',
+                     border=False):
     packet = io.BytesIO()
     can = canvas.Canvas(packet, pagesize=overlay_page_size)
-    can.setFont('Helvetica', 10)
-    can.setFillColorRGB(0, 0, 00, alpha=1)
+    can.setFont(font, font_size)
+    can.setFillColorRGB(0, 0, 0, alpha=1)
     can.saveState()
-    can.drawCentredString(100, 5, text)
+    if border:
+        can.setLineWidth(3)
+        can.setStrokeAlpha(0.5)
+        height = 60
+        width = 200
+        padding = 10
+        can.roundRect(x - padding, y - padding - height, width + 2 * padding, height + 2 * padding, 5)
+    lin_space = 20
+    if y is None:
+        y = int(overlay_page_size[1])
+    for line in text.split('\n'):
+        can.drawString(x, y - lin_space, line)
+        lin_space += 15
     can.restoreState()
     can.save()
     packet.seek(0)
     return packet
 
 
+def outside_delivery():
+    cursor.execute('SELECT pay_method FROM csv_data WHERE order_number = ?', (order_id,))
+    if cursor.fetchone()[0] != 'COD':
+        return False
+    delivery = 0
+    cursor.execute('SELECT shipping_cost FROM csv_data WHERE order_number = ?', (order_id,))
+    for single_shipping_fee in cursor.fetchall():
+        delivery += single_shipping_fee[0]
+    cursor.execute('SELECT shipping_city FROM csv_data WHERE order_number = ?', (order_id,))
+    is_dhaka = 'D*h' in cursor.fetchone()
+    if (55 >= delivery > 0 and is_dhaka) or delivery >= 110:
+        try:
+            voucher = float(page_text.split('Voucher:')[1].splitlines()[0].replace('-', ''))
+        except IndexError:
+            print("Voucher value couldn't extracted, set to 0")
+            voucher = 0
+        if voucher == 0:
+            cursor.execute(
+                'INSERT INTO outside_delivery (delivery_charge, is_dhaka, order_number) VALUES ( ?, ?, ?)',
+                (delivery, is_dhaka, order_id))
+            return True
+
+
+# Variable
 directory = 'D:\\Downloads\\Daraz Invoice'
-output_filename = 'merged.pdf'
-if os.path.exists(os.path.join(directory, output_filename)):
-    os.remove(os.path.join(directory, output_filename))
-if os.path.exists(os.path.join(directory, 'Final.pdf')):
-    os.remove(os.path.join(directory, 'Final.pdf'))
+merged_pdf = os.path.join(directory, 'merged.pdf')
+final_file = os.path.join(directory, 'Final.pdf')
+CLEAR = 2
+
+if os.path.exists(merged_pdf):
+    os.remove(merged_pdf)
+if os.path.exists(final_file):
+    os.remove(final_file)
 
 # Create a PDF merger object
 pdf_merger = PyPDF2.PdfMerger()
@@ -43,42 +117,86 @@ for pdf in pdf_files:
     pdf_merger.append(pdf_path)
 
 # Write out the merged PDF
-with open(os.path.join(directory, output_filename), 'wb') as output_file:
+with open(os.path.join(directory, merged_pdf), 'wb') as output_file:
     pdf_merger.write(output_file)
 
-order: list = []
-label: list = []
+# Working with xlsx file
 for file in os.listdir(directory):
-    if file.endswith('.csv'):
-        with open(os.path.join(directory, file), 'r', newline='\n', encoding='utf-8') as csv_file:
-            reader = csv.reader(csv_file)
-            header = next(reader)
-            for row in reader:
-                order_id = row[0].split(';')[8]
-                sku = row[0].split(';')[4]
-                order.append((order_id, sku))
-                print(order_id, sku)
+    if file.endswith('.xlsx'):
+        reader = pd.read_excel(os.path.join(directory, file), sheet_name='sheet1')
+        for _, row in reader.iterrows():
+            order_id = row['orderNumber']
+            if row['status'] != 'ready_to_ship':
+                print('Ready to ship:', order_id)
+                raise Exception
+            sku = row['sellerSku']
+            original_sku = identical(identical(sku.split('-')[0]) + '-' + identical(sku.split('-')[1].split('_')[0]))
+            order_time = datetime.strptime(row['createTime'], '%d %b %Y %H:%M')
+            shipping_cost = row['shippingFee']
+            unit_price = row['unitPrice']
+            pay_method = row['payMethod']
+            shipping_city = row['shippingCity']
+            cursor.execute('INSERT INTO csv_data VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+                           (order_id, sku, order_time.timestamp(), shipping_cost, unit_price, pay_method,
+                            shipping_city, original_sku.title()))
+            # Minimal pricing check
+            cursor.execute('SELECT price FROM pricing WHERE product_tag = ?', (original_sku.split('-')[0],))
+            min_price = cursor.fetchone()[0] * (1 - CLEAR / 100)
+            if unit_price < min_price:
+                print(f'Price is less than expected ({order_id}): {unit_price} ({min_price})')
+conn.commit()
 
+# Insert to pdf_data database
+# cursor.execute('INSERT INTO pdf_data (order_number, identity_sku) '
+#                'SELECT order_number, GROUP_CONCAT(identical_sku) FROM csv_data GROUP BY order_number')
+# conn.commit()
+
+# Working with pdf file
 pw = PyPDF2.PdfWriter()
-pdf_path = 'D:\\Downloads\\Daraz Invoice\\merged.pdf'
-with open(pdf_path, 'rb') as file:
+with open(merged_pdf, 'rb') as file:
     reader = PyPDF2.PdfReader(file)
     for page_num in range(len(reader.pages)):
         page = reader.pages[page_num]
         page_size = (page.mediabox.width, page.mediabox.height)
+        page_text = page.extract_text()
+        try:
+            order_id = page_text.split('Tracking Number')[1].split('COD')[1].split('\n')[1]
+        except IndexError:
+            if page.extract_text() != '':
+                print(f'Page no {page_num} Skipped =>', "Can't find order number")
+            else:
+                print(f'Page no {page_num} Skipped')
+            order_id = ''
+        cursor.execute('SELECT sku FROM override_sku WHERE order_number = ?', (order_id,))
+        sku = cursor.fetchall()
+        if not sku:
+            cursor.execute('SELECT identical_sku FROM csv_data WHERE order_number = ?', (order_id,))
+            sku = cursor.fetchall()
+        try:
+            quantity = int(page_text.split('Item Quantity:')[1].split('Order Creation')[0])
+        except IndexError:
+            print("Can't find quantity")
+            sku_str = 'Error quantity: ' + ','.join(x[0] for x in sku)
+        else:
+            if len(sku) != quantity and len(sku) != 0:
+                print(f'Item Quantity is {quantity} but found {len(sku)}')
+                sku_str = 'Error quantity: ' + ','.join(x[0] for x in sku)
+            else:
+                sku_str = ','.join(x[0] for x in sku)
+        try:
+            phone = page_text.split('Phone: ')[1].splitlines()[0]
+        except IndexError:
+            print("Can't find phone number")
+            sku_str = 'Error phone: ' + ','.join(x[0] for x in sku)
+            phone = order_id
+        cursor.execute('INSERT INTO pdf_data (order_number, seller_sku, page_number, phone) VALUES (?, ?, ?, ?)',
+                       (order_id, sku_str, page_num, phone))
+        print(order_id, sku_str)
+        # Outside delivery
+        outside_delivery()
 
-        order_id = page.extract_text().split('COD')[1].split('\n')[1]
-        quantity = int(page.extract_text().split('Item Quantity:')[1].split(' ')[0])
-        for _ in range(quantity):
-            for csv_order_id, sku in order:
-                # print(csv_order_id, order_id)
-                if csv_order_id == order_id:
-                    label.append((page_num, sku))
-                    print(csv_order_id, sku)
-                    order.remove((order_id, sku))
-                    break
         # Create a new PDF with the watermark
-        watermark_pdf_data = create_watermark(sku, page_size)
+        watermark_pdf_data = create_watermark(sku_str, page_size)
         watermark_pdf = PyPDF2.PdfReader(watermark_pdf_data)
         watermark_page = watermark_pdf.pages[0]
 
@@ -87,17 +205,60 @@ with open(pdf_path, 'rb') as file:
 
         # Add the merged page to the PDF writer
         pw.add_page(page)
+    conn.commit()
 
 # Write the final PDF to a file
-with open(pdf_path, 'wb') as output_pdf:
+with open(merged_pdf, 'wb') as output_pdf:
     pw.write(output_pdf)
 
-label = sorted(label, key=sort_factory)
-print(label)
-with open(pdf_path, 'rb') as file:
+# Duplicate order
+for row in cursor.execute('SELECT COUNT(*), phone FROM pdf_data GROUP BY phone').fetchall():
+    if row[0] > 1:
+        cursor.execute('SELECT order_number FROM pdf_data WHERE phone = ?', (row[1],))
+        print('Duplicate order: ' + ','.join(x[0] for x in cursor.fetchall()))
+
+cursor.execute('SELECT order_number, seller_sku, page_number FROM pdf_data')
+label = sorted(cursor.fetchall(), key=sort_factory)
+
+# Print order details on first page
+pw = PyPDF2.PdfWriter()
+cursor.execute('SELECT COUNT(*) FROM csv_data')
+brief_heading = f'\tOrder Brief (Total: {cursor.fetchone()[0]})'
+brief_heading_page = PyPDF2.PdfReader(create_watermark(brief_heading, landscape(A5), x=30, font_size=12))
+count = 0
+cursor.execute('SELECT identity_sku, COUNT(identity_sku) AS count FROM pdf_data GROUP BY identity_sku')
+brief = ''
+for row in cursor.fetchall():
+    brief += f'{row[0]} : {str(row[1])} ({count + 1} - {row[1] + count})' + '\n'
+    count += row[1]
+index_page = PyPDF2.PdfReader(create_watermark(brief, landscape(A5), 30, y=page_size[1] - 15))
+index_page.pages[0].merge_page(brief_heading_page.pages[0])
+pw.add_page(index_page.pages[0])
+current_date = datetime.now().strftime('%d %B %Y')
+print('Sorting page', end='')  # Progress bar initialization
+outside_delivery = ''
+safety_text = "  wfZ‡i Kv‡Pi wRwbm Av‡Q\n\nZvB e· †P‡c †QvU Ki‡eb bv"
+with open(merged_pdf, 'rb') as file:
     reader = PyPDF2.PdfReader(file)
-    pw = PyPDF2.PdfWriter()
-    for page_num, sku in label:
-        pw.add_page(reader.pages[page_num])
-with open('D:\\Downloads\\Daraz Invoice\\Final.pdf', 'wb') as file:
+    for index, (order_id, _, page_num) in enumerate(label):
+        # Outside delivery
+        cursor.execute('SELECT * FROM outside_delivery WHERE order_number = ?', (order_id,))
+        if cursor.fetchall():
+            cursor.execute('UPDATE outside_delivery SET page_number = ? WHERE order_number = ?',
+                           (index + 2, order_id))
+        print(' *', end='')  # Progress bar
+        page = reader.pages[page_num]
+        # Print date, page number and safety text
+        page_size = (page.mediabox.width, page.mediabox.height)
+        page.merge_page(PyPDF2.PdfReader(create_watermark(str(current_date), page_size, y=30)).pages[0])
+        page.merge_page(PyPDF2.PdfReader(create_watermark(
+            str(index + 1), page_size, page_size[0] - 30, 30, 12)).pages[0])
+        safety_page = PyPDF2.PdfReader(create_watermark(safety_text, page_size, page_size[0] / 2 + 20, y=0,
+                                                        font_size=20, font='BanglaFont', border=True)).pages[0]
+        safety_page.add_transformation(Transformation().rotate(15))
+        safety_page.add_transformation(Transformation().translate(30, -10))
+        page.merge_page(safety_page)
+        pw.add_page(page)
+conn.commit()
+with open(final_file, 'wb') as file:
     pw.write(file)
